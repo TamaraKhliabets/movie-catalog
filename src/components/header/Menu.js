30df247ef96f5cd58090362e592c3ff5207bc2a3
Toma Khliabets <tokotomka@gmail.com>
2018-11-06 14:57:00 +0300

    Test pre-commit

--- src/components/header/Menu.js
+++ src/components/header/Menu.js
@@ -5,7 +5,7 @@ import menu from '../../default/menu.png';
 
 export default class Menu extends Component {
 		state = {
-		  isMobile: true,
+		  isMobile: false,
 		};
 
 		componentDidMount() {
@@ -33,9 +33,9 @@ export default class Menu extends Component {
 
 		  return (
 			<div className="menu">
-				<button type="button" className="menu_button" onClick={this.changeVisibleMenu}>
-					<img src={menu} />
-				</button>
+				{/*<button type="button" className="menu_button" onClick={this.changeVisibleMenu}>*/}
+					{/*<img src={menu} />*/}
+				{/*</button>*/}
 				{
 						!isMobile
 						  ? (
